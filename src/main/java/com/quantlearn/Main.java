1cd0d49e8ab1634903a107c881f49b65f64da23d
Pranav <pran9v@gmail.com>
2025-06-30 18:14:54 +0530

    Remove .class files and update .gitignore

--- src/main/java/com/quantlearn/Main.java
+++ src/main/java/com/quantlearn/Main.java
@@ -1,8 +1,13 @@
 package com.quantlearn;
 
+import com.quantlearn.backtest.BacktestEngine;
 import com.quantlearn.backtest.IDataHandler;
+import com.quantlearn.backtest.IExecutionHandler;
+import com.quantlearn.backtest.IPortfolio;
 import com.quantlearn.backtest.IStrategy;
 import com.quantlearn.backtest.data.CsvDataHandler;
+import com.quantlearn.backtest.execution.SimulatedExecutionHandler;
+import com.quantlearn.backtest.portfolio.BasicPortfolio;
 import com.quantlearn.strategy.BuyAndHoldStrategy;
 
 public class Main {
@@ -16,6 +21,19 @@ public class Main {
 
         IStrategy strategy = new BuyAndHoldStrategy(symbol);
 
-        //IPortfolio portfolio = new 
+        IPortfolio portfolio = new BasicPortfolio(initialCash);
+        
+        IExecutionHandler executionHandler = new SimulatedExecutionHandler();
+
+        BacktestEngine engine = new BacktestEngine(
+            dataHandler,
+            executionHandler,
+            portfolio,
+            strategy
+        );
+
+        engine.run();
+
+        System.out.println("--- Backtest complete ---");
     }
 }
